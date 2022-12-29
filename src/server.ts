cc399e9f91c6052419c77854d11f4673dec26d55
Mariam Lawal <lawalmariamyetunde@gmail.com>
2022-12-29 17:45:24 +0100

    Base files

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import * as bodyParser from 'body-parser';
+
+
+const app = express();
+
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+const PORT = 3000;
+
+app.listen(PORT, () => {console.log(`Server is running on http://localhost:${PORT}`)})
+
+
+//Load Routers
+// app.use('/', require('./routes'))
+
+app.get('/api', (req, res) => {
+    res.send('Hello World!');
+});
